9720691a6ace693147a4452da6455ac71e415ca3
Astha <sweetastha23@gmail.com>
2013-11-10 13:35:29 +0530

    handling file not found

--- server/node.cpp
+++ server/node.cpp
@@ -90,132 +90,159 @@ node::node(){
 
 			//Check if this request belongs to this server
   		if(bigModulo(mf,N) == id){
-				//start a tcp connection and process the request depending upon its type i,e store or get
-  			printf("[Server] Establishing TCP connection with client...\n");
-
-  			int sockfd; 
-				struct sockaddr_in remoteClient_addr; // will hold the destination address 
-				if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1){ // TCP connection
-					printf("[ERROR] Could not create TCP socket. Exiting!!\n");
-				exit(0);
-				} 
-				remoteClient_addr.sin_family = AF_INET; // host byte order 
-				remoteClient_addr.sin_port = htons(port); // network byte order 
-				remoteClient_addr.sin_addr.s_addr = inet_addr(ip); // Destination IP address
-				memset(&(remoteClient_addr.sin_zero), '\0', 8); // zero the rest of the struct 
-
-				// Establish connection
-				if (connect(sockfd, (struct sockaddr *)&remoteClient_addr, sizeof(struct sockaddr)) == -1){
-					printf("[ERROR] Could not connect to client. Please check connections!!\n");
-					//exit(0);
-				} 	
-				else {
-					printf("[Server] Connected successfully!\n");
-				}
 
 				// Process a store request
-				if (strcmp(requestType,  "store") == 0){
-					int success = 0;
-					while(success == 0)
-					{
+        if (strcmp(requestType,  "store") == 0){
+          //start a tcp connection and process the request depending upon its type i,e store or get
+          printf("[Server] Establishing TCP connection with client...\n");
+
+          int sockfd; 
+        struct sockaddr_in remoteClient_addr; // will hold the destination address 
+        if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1){ // TCP connection
+          printf("[ERROR] Could not create TCP socket. Exiting!!\n");
+          exit(0);
+        } 
+        remoteClient_addr.sin_family = AF_INET; // host byte order 
+        remoteClient_addr.sin_port = htons(port); // network byte order 
+        remoteClient_addr.sin_addr.s_addr = inet_addr(ip); // Destination IP address
+        memset(&(remoteClient_addr.sin_zero), '\0', 8); // zero the rest of the struct 
+
+        // Establish connection
+        if (connect(sockfd, (struct sockaddr *)&remoteClient_addr, sizeof(struct sockaddr)) == -1){
+          printf("[ERROR] Could not connect to client. Please check connections!!\n");
+          //exit(0);
+        }   
+        else {
+          printf("[Server] Connected successfully!\n");
+        }
+
+
+        int success = 0;
+        while(success == 0)
+        {
 						// Start receiving file, continue recieving till success is 1
-						string path = nData.folderPath.append("/");
-						string finalPath = path.append(mf);
-						char* write_file = (char *)finalPath.c_str();
-						char receive_buffer[LENGTH];
-						FILE *fileOpen = fopen(write_file, "w+");
-						if(fileOpen == NULL){
-							printf("[ERROR] Cannot open the file %s to write.\n", write_file);
-							exit(0);							
-						}
-						else{
-							bzero(receive_buffer, LENGTH); 
-							int recv_block_size = 0;
+          string path = nData.folderPath.append("/");
+          string finalPath = path.append(mf);
+          char* write_file = (char *)finalPath.c_str();
+          char receive_buffer[LENGTH];
+          FILE *fileOpen = fopen(write_file, "w+");
+          if(fileOpen == NULL){
+            printf("[ERROR] Cannot open the file %s to write.\n", write_file);
+            exit(0);							
+          }
+          else{
+            bzero(receive_buffer, LENGTH); 
+            int recv_block_size = 0;
 							// Try writing to file whenever a new packet is recieved
-							while((recv_block_size = recv(sockfd, receive_buffer, LENGTH, 0)) > 0) {
-								int write_sz = fwrite(receive_buffer, sizeof(char), recv_block_size, fileOpen);
-								if(write_sz < recv_block_size){
-									printf("[ERROR] File write failed on server.\n");
-									exit(0);
-								}
-								bzero(receive_buffer, LENGTH);
-							}
-							if(recv_block_size < 0){
-								if (errno == EAGAIN){
+            while((recv_block_size = recv(sockfd, receive_buffer, LENGTH, 0)) > 0) {
+              int write_sz = fwrite(receive_buffer, sizeof(char), recv_block_size, fileOpen);
+              if(write_sz < recv_block_size){
+                printf("[ERROR] File write failed on server.\n");
+                exit(0);
+              }
+              bzero(receive_buffer, LENGTH);
+            }
+            if(recv_block_size < 0){
+              if (errno == EAGAIN){
            				 // Timeout Error
-									printf("[ERROR] recv() timed out.\n");
-									exit(0);
-								}
-								else{
-            			// Other errors
-									printf("[ERROR] recv() failed due to errno = %d\n", errno);
-									exit(0);
-								}
-							}
-							printf("[Server] File received at Server!\n");
-							fclose(fileOpen); 
-						}
-						success = 1;
+               printf("[ERROR] recv() timed out.\n");
+               exit(0);
+             }
+             else{
+            // Other errors
+               printf("[ERROR] recv() failed due to errno = %d\n", errno);
+               exit(0);
+             }
+           }
+           printf("[Server] File received at Server!\n");
+           fclose(fileOpen); 
+         }
+         success = 1;
 						// Close connection with Client
-						close(sockfd);
-						printf("[Server] Connection with Client closed. Server will wait now...\n");
-					}
+         close(sockfd);
+         printf("[Server] Connection with Client closed. Server will wait now...\n");
+       }
 
 
-				}
-				else if (strcmp(requestType,  "get") == 0){
-					string path = nData.folderPath.append("/");
-					string finalPath = path.append(mf);
-					char* write_file = (char *)finalPath.c_str();
-					char send_buffer[LENGTH];
+     }
+     else if (strcmp(requestType,  "get") == 0){
+       string path = nData.folderPath.append("/");
+       string finalPath = path.append(mf);
+       char* write_file = (char *)finalPath.c_str();
+       char send_buffer[LENGTH];
 					// Open file to read
-					FILE *fileOpen = fopen(write_file, "r");
+       FILE *fileOpen = fopen(write_file, "r");
 
 					// Give error if file cannot be opened
-					if(fileOpen == NULL)
-					{
-						printf("ERROR: File %s not found.\n", write_file);
-						exit(0);
-					}
+       if(fileOpen == NULL)
+       {
+        printf("ERROR: File %s not found.\n", write_file);
+        // exit(0);
+       }
+       else {
+        //start a tcp connection and process the request depending upon its type i,e store or get
+        printf("[Server] Establishing TCP connection with client...\n");
 
-					printf("[Server] Sending %s to the Client... ", write_file);
-					bzero(send_buffer, LENGTH); 
-					int block_size; 
-					int flag = true;
-					// Create chunks of the file, and send them sequentially to server (node)
+        int sockfd; 
+        struct sockaddr_in remoteClient_addr; // will hold the destination address 
+        if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1){ // TCP connection
+          printf("[ERROR] Could not create TCP socket. Exiting!!\n");
+          exit(0);
+        } 
+        remoteClient_addr.sin_family = AF_INET; // host byte order 
+        remoteClient_addr.sin_port = htons(port); // network byte order 
+        remoteClient_addr.sin_addr.s_addr = inet_addr(ip); // Destination IP address
+        memset(&(remoteClient_addr.sin_zero), '\0', 8); // zero the rest of the struct 
 
-					while((block_size = fread(send_buffer, sizeof(char), LENGTH, fileOpen)) > 0)
-					{
-						if(send(sockfd, send_buffer, block_size, 0) < 0)
-						{
-							printf( "[ERROR] Failed to send file %s. (errno = %d)\n", write_file, errno);
-							flag = false;
-							break;
-						}
-						bzero(send_buffer, LENGTH);
-					}
-					// File has been sent successfully.
-					if (flag){
-						printf("[Server] File %s from is Sent!\n", write_file);
-					}
-					 // Close the connection
-					close(sockfd);
-					printf("[Server] Connection with Client closed. Server will wait now...\n");
+        // Establish connection
+        if (connect(sockfd, (struct sockaddr *)&remoteClient_addr, sizeof(struct sockaddr)) == -1){
+          printf("[ERROR] Could not connect to client. Please check connections!!\n");
+          //exit(0);
+        }   
+        else {
+          printf("[Server] Connected successfully!\n");
+        }
+        printf("[Server] Sending %s to the Client... ", write_file);
+        bzero(send_buffer, LENGTH); 
+        int block_size; 
+        int flag = true;
+          // Create chunks of the file, and send them sequentially to server (node)
 
-				}
-			}
-			else{
+        while((block_size = fread(send_buffer, sizeof(char), LENGTH, fileOpen)) > 0)
+        {
+          if(send(sockfd, send_buffer, block_size, 0) < 0)
+          {
+            printf( "[ERROR] Failed to send file %s. (errno = %d)\n", write_file, errno);
+            flag = false;
+            break;
+          }
+          bzero(send_buffer, LENGTH);
+        }
+          // File has been sent successfully.
+        if (flag){
+          printf("[Server] File %s from is Sent!\n", write_file);
+        }
+           // Close the connection
+        close(sockfd);
+        printf("[Server] Connection with Client closed. Server will wait now...\n");
+
+      }
+
+
+    }
+  }
+  else{
 				//forward the packet to correct destination
-				memset((char *)&remoteAddr, 0, sizeof(remoteAddr));
-				remoteAddr.sin_family = AF_INET;
-				remoteAddr.sin_addr.s_addr = inet_addr(cloudNodesData[bigModulo(mf,N)].ipAddress.c_str());
-				remoteAddr.sin_port = htons(cloudNodesData[bigModulo(mf,N)].portNo);
-				printf ("[Server] Forwarding request to correct node... \n");
-				if (sendto(mySocket, receivedData, strlen(receivedData), 0, (struct sockaddr *)&remoteAddr, sizeof(remoteAddr)) < 0){
-					printf("[Error] Could not forward request!! Exiting!\n");
-					exit(0);
-				}
-			}
-		}	
-	}
+    memset((char *)&remoteAddr, 0, sizeof(remoteAddr));
+    remoteAddr.sin_family = AF_INET;
+    remoteAddr.sin_addr.s_addr = inet_addr(cloudNodesData[bigModulo(mf,N)].ipAddress.c_str());
+    remoteAddr.sin_port = htons(cloudNodesData[bigModulo(mf,N)].portNo);
+    printf ("[Server] Forwarding request to correct node... \n");
+    if (sendto(mySocket, receivedData, strlen(receivedData), 0, (struct sockaddr *)&remoteAddr, sizeof(remoteAddr)) < 0){
+     printf("[Error] Could not forward request!! Exiting!\n");
+     exit(0);
+   }
+ }
+}	
+}
 }
